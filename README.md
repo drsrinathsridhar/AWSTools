e3b3e9f878e79aa45c575e0e2e6d76f0a5a98b8b
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-01-09 18:34:07 -0800

    Progressing with scripts

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # AWSTools
-Some scripts and tools for running jobs on AWS.
+Some scripts and tools for running deep learning jobs on AWS.
+
+## Requirements
+- Linux
+- Python 3+
+- AWS account ID and key
+- Enough credits to create GPU instances (these are expensive, beware!)
+
+## Getting Started
+
+### Initial Setup
+1. Setup your AWS CLI tools by running ``aws configure``. See the [AWS CLI documentation][1] for details.
+2. [ Optional ]: Setup shared EFS storage space for **data**---input datasets, output models and weights, and logs (including console outputs).
+
+### [ Optional ]: EFS Setup
+
+TODO
+
+### Project Setup
+
+For each machine learning project, we create a subnet which can contain dedicated instances that are all stopped and started on demand to run tasks.
+All these instances come with an EBS-backed root and can preserve any local data.
+Optionally, datasets used for training and output of training are all written to the common EFS storage setup as described above.
+
+For each project, run the following commands **once** at the beginning.
+
+``python startDLProject.py --help``
+
+# Contact
+Srinath Sridhar  
+<http://srinathsridhar.com>  
+<ssrinath@cs.stanford.edu>
+
+[1]: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
---
+++ startProject.py
@@ -0,0 +1,33 @@
+import os, sys, subprocess, argparse
+
+Parser = argparse.ArgumentParser(description='Initial AWS setup for a deep learning project.')
+
+# --------------------
+# Required Arguments
+# --------------------
+Parser.add_argument('--project-name', help='Provide a name for this deep learning project.', required=True)
+Parser.add_argument('--key-name', help='Provide an SSH key name to be used for creating instances.', required=True)
+# --------------------
+# Optional Arguments
+# --------------------
+Parser.add_argument('--count', help='How many instance counts to create. Default is 1.', required=False, default=1, type=int)
+Parser.add_argument('--security-group', help='Provide an optional security-group. If not provided, ''default'' will be used.', required=False, default='default')
+Parser.add_argument('--image-id', help='Provide an AMI image ID. Default is ''ami-0c9ae74667b049f59''.', required=False, default='ami-0c9ae74667b049f59')
+Parser.add_argument('--instance-type', help='Provide an instance type. Default is ''p3.2xlarge''.', required=False, default='p3.2xlarge')
+
+
+if __name__ == '__main__':
+    if 'linux' not in str(sys.platform):
+        raise RuntimeError('Unsupported OS. Only Linux is supported.')
+
+    Args = Parser.parse_args()
+    ArgsDict = vars(Args)
+    for Arg in ArgsDict:
+        ArgVal = ArgsDict[Arg]
+        if ArgsDict[Arg] is None:
+            ArgVal = 'None'
+        print('{:<15}:   {:<50}'.format(Arg, ArgVal))
+
+    # 'aws ec2 run-instances --image-id ami-0c9ae74667b049f59 --count 1 --instance-type p3.2xlarge --key-name ssrinath_aws --security-groups default'
+    subprocess.call(['df', '-h'])
+
