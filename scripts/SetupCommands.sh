425ddb4ef18e2e49fc342f21e7814d9b4eaa7fe1
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-05-04 15:45:27 +0200

    soft link update

--- scripts/SetupCommands.sh
+++ scripts/SetupCommands.sh
@@ -9,8 +9,8 @@ cd ~
 sudo mkdir /media/efs
 sudo chmod go+rw /media/efs/
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-7646d70f.efs.us-east-2.amazonaws.com:/ /media/efs
-ln -s /media/efs/input/
-ln -s /media/efs/output/
+ln -s /media/efs/input ~/input
+ln -s /media/efs/output ~/output
 source activate pytorch_p36
 conda install opencv
 pip install palettable
