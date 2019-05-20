9ea7e21eb217c4db7de79032813bb1e8e3865bfc
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-05-20 15:04:34 +0200

    MAx I/O EFS

--- scripts/SetupCommands.sh
+++ scripts/SetupCommands.sh
@@ -26,3 +26,6 @@ sudo mkdir /media/CatReconEFS
 sudo chmod go+rw /media/CatReconEFS/
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-5c3a4825.efs.us-east-2.amazonaws.com:/ /media/CatReconEFS
 
+sudo mkdir /media/CatReconEFS_COPY
+sudo chmod go+rw /media/CatReconEFS_COPY
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-7317530a.efs.us-east-2.amazonaws.com:/ /media/CatReconEFS_COPY
