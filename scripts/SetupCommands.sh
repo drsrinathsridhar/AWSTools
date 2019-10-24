347172fabe4455916ff35efb780998c461280dfb
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-10-23 17:36:50 -0700

    HNOCS commands

--- scripts/SetupCommands.sh
+++ scripts/SetupCommands.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+
+###############
+# H-NOCS
+###############
+wget -O .bashrc.user https://raw.githubusercontent.com/drsrinathsridhar/home/stanford/.bashrc.user
+wget -O .bashrc https://raw.githubusercontent.com/drsrinathsridhar/home/stanford/.bashrc
+source .bashrc
+mkdir code
+cd code/
+# OPTIONAL
+git clone https://github.com/drsrinathsridhar/CatRecon
+cd CatRecon
+git config credential.helper store
+cd ~
+sudo mkdir /media/efs
+sudo chmod a+rw /media/efs/
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-fe744587.efs.us-east-2.amazonaws.com:/ /media/efs
+ln -s /media/efs/input ~/input
+ln -s /media/efs/output ~/output
+source activate pytorch_p36
+conda install opencv
+pip install palettable gputil
+pip install git+https://github.com/drsrinathsridhar/tk3dv.git
+
+[ Optional ]
+pip install --upgrade pip
+nano /etc/fstab
+fs-fe744587:/ /media/efs efs defaults,_netdev 0 0
+
+
+###############
+# X-NOCS
+###############
 wget -O .bashrc.user https://raw.githubusercontent.com/drsrinathsridhar/home/stanford/.bashrc.user
 wget -O .bashrc https://raw.githubusercontent.com/drsrinathsridhar/home/stanford/.bashrc
 source .bashrc
